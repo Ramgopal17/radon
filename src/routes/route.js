7ce6e2cfdc35420bfcddb217dc039db3233efa12
ramgopal17 <ramgkp6@gmail.com>
2022-06-02 22:54:01 +0530

    problem4

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const externalModule1 = require('../logger/logger.js')
 const externalModule2 = require('../util/helper.js')
 const externalModule3 = require('../validator/formatter.js')
-
+const lodash =require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -35,6 +35,19 @@ router.get('/test-me3', function (req, res) {
 
 router.get('/test-me4', function (req, res) {
     res.send('My last api!')
+});
+
+router.get('/hello', function (req, res) {
+    let arr=lodash.chunk(['January','February','March','April','May','June','July','August','September','October','November','December'],3);
+    console.log(arr)
+    let odd_num =lodash.tail([1,3,5,7,9,11,13,15,17,19]);
+    console.log('the last 9 element is'+    odd_num);
+    let non_duplicate=lodash.union([1,2],[2,3,4],[3,4,5,6],[6,7,8],[7,8,9,10]);
+    console.log('Merged array is '+non_duplicate);
+    let movie_lib=lodash.fromPairs([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]])
+    console.log("the object containing key value pair is"+  JSON.stringify( movie_lib ))
+    res.send('My last api!')
+    
 });
 
 module.exports = router;
