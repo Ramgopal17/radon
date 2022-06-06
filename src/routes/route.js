40e2923b63b950a0edb98a564c597f0ce1452075
ramgopal17 <ramgkp6@gmail.com>
2022-06-06 11:59:06 +0530

    mongodb assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+
+const UserController= require("../controllers/userController.js")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createNewBook", UserController.createNewBook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+
+
+router.get("/getbooklist", UserController.getbooklist)
+
+
+
 
 module.exports = router;
